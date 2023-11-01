63820051603e58be21d6dec0fb8c6fe43e521c9e
Jakub Mikołajczyk <kmikolaj@gmail.com>
2023-11-01 22:14:55 +0100

    update mpv

--- private_dot_config/private_mpv/lua-modules/auto-profiles-functions.lua
+++ private_dot_config/private_mpv/lua-modules/auto-profiles-functions.lua
@@ -4,24 +4,30 @@ local msg = require 'mp.msg'
 local exec_cache = {}
 
 local function exec(process, force_exec)
-		local key = table.concat(process, " ")
-		if force_exec or exec_cache[key] == nil or exec_cache[key].error then
-				local p_ret = utils.subprocess({args = process, playback_only = false})
-				exec_cache[key] = p_ret
-				if p_ret.error and p_ret.error == "init" then
-						msg.error("executable not found: " .. key)
-				end
-				return p_ret
-		else
-				return exec_cache[key]
+	local key = table.concat(process, " ")
+	if force_exec or exec_cache[key] == nil or exec_cache[key].error then
+		local p_ret = utils.subprocess({args = process, playback_only = false})
+		exec_cache[key] = p_ret
+		if p_ret.error and p_ret.error == "init" then
+			msg.error("executable not found: " .. key)
 		end
+		return p_ret
+	else
+		return exec_cache[key]
+	end
 end
 
 function is_remote_filesystem(path)
-	if path ~= nil then
-		local result = exec({'stat', '-f', '-c', '%T', path})
-		local fs = result.stdout:gsub("%s+", "")
-		return fs == "nfs"
+	if path == nil then
+		return false
 	end
-	return false
+
+	local is_url = string.match(path, '[a-z]*://[^ >,;]*') ~= nil
+	if is_url then
+		return false
+	end
+
+	local result = exec({'stat', '-f', '-c', '%T', path})
+	local fs = result.stdout:gsub("%s+", "")
+	return fs == "nfs"
 end
